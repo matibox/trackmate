92b0dd991182cc622d3b7e0f4a4108b7ecb8bdb1
Matibox <mateusz.hladky@gmail.com>
2023-01-24 16:45:29 +0100

    Add /login page Protect login and index routes

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: "avoid",
 };
