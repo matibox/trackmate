2723106e27e31cda2b87777f4528ceb3ff962ad2
Matibox <mateusz.hladky@gmail.com>
2023-01-26 14:30:44 +0100

    Add basic Navbar Add more styles to Button component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { ArrowLeftOnRectangleIcon } from '@heroicons/react/20/solid';
+import Image from 'next/image';
+import { type FC } from 'react';
+import Button from './ui/Button';
+
+const Navbar: FC = () => {
+  return (
+    <nav className='flex h-[var(--navbar-height)] items-center border-b border-slate-700 bg-slate-800 px-4'>
+      <Image src='/Mono.png' alt='' width={50} height={50} />
+      <Button intent='secondary' size='small' className='ml-auto' gap='small'>
+        <span>Sign out</span>
+        <ArrowLeftOnRectangleIcon className='h-[18px]' />
+      </Button>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,12 +3,14 @@ import { type ButtonHTMLAttributes, type ReactNode, type FC } from 'react';
 import cn from '../../lib/classes';
 
 const buttonStyles = cva(
-  'group px-6 py-2 rounded flex justify-center items-center transition',
+  'group rounded flex justify-center items-center transition',
   {
     variants: {
       intent: {
         primary:
           'ring-1 ring-sky-400 bg-sky-500 text-slate-50 font-semibold hover:bg-sky-400',
+        secondary:
+          'ring-1 ring-slate-600 text-slate-50 bg-slate-700 hover:bg-slate-600 hover:ring-slate-500',
       },
       fullWidth: {
         true: 'w-full',
@@ -17,9 +19,14 @@ const buttonStyles = cva(
         small: 'gap-1',
         normal: 'gap-2',
       },
+      size: {
+        small: 'px-4 py-1 text-sm',
+        normal: 'px-6 py-2',
+      },
     },
     defaultVariants: {
       gap: 'normal',
+      size: 'normal',
     },
   }
 );
@@ -35,12 +42,13 @@ const Button: FC<ButtonProps> = ({
   intent,
   fullWidth,
   gap,
+  size,
   className,
   ...props
 }) => {
   return (
     <button
-      className={cn(buttonStyles({ intent, fullWidth, gap }), className, {
+      className={cn(buttonStyles({ intent, fullWidth, gap, size }), className, {
         'cursor-not-allowed bg-sky-900 text-slate-400 ring-sky-800 hover:bg-sky-900 hover:text-slate-400 hover:ring-sky-800':
           props.disabled,
       })}
