d73b558841f97cbd23be025f8e8ab5cb93a6ac52
Matibox <mateusz.hladky@gmail.com>
2023-01-27 14:52:34 +0100

    Fix sign out button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,19 @@ import { ArrowLeftOnRectangleIcon } from '@heroicons/react/20/solid';
 import Image from 'next/image';
 import { type FC } from 'react';
 import Button from '@ui/Button';
+import { signOut } from 'next-auth/react';
 
 const Navbar: FC = () => {
   return (
     <nav className='flex h-[var(--navbar-height)] items-center border-b border-slate-700 bg-slate-800 px-4'>
       <Image src='/Mono.png' alt='' width={50} height={50} />
-      <Button intent='secondary' size='small' className='ml-auto' gap='small'>
+      <Button
+        intent='secondary'
+        size='small'
+        className='ml-auto'
+        gap='small'
+        onClick={() => void signOut()}
+      >
         <span>Sign out</span>
         <ArrowLeftOnRectangleIcon className='h-[18px]' />
       </Button>
