123a4de4cfec75ecaf41c6205a0f882b9d4912c2
Matibox <mateusz.hladky@gmail.com>
2023-02-22 16:06:04 +0100

    Add unread notifications indicator Fix navbar buttons accessibility issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { signOut } from 'next-auth/react';
 import Link from 'next/link';
 import { BellIcon, Cog8ToothIcon } from '@heroicons/react/24/outline';
 import { useSettingsStore } from '../store/useSettingsStore';
+import cn from '../lib/classes';
 
 const Navbar: FC = () => {
   const { open: openSettings } = useSettingsStore();
@@ -13,7 +14,7 @@ const Navbar: FC = () => {
   return (
     <nav className='flex h-[var(--navbar-height)] items-center gap-4 border-b border-slate-700 bg-slate-800 px-4'>
       <Link href='/' aria-label='dashboard'>
-        <Image src='/Mono.png' alt='' width={50} height={50} />
+        <Image src='/Mono.png' alt='Logo' width={50} height={50} />
       </Link>
       <Button
         intent='secondary'
@@ -27,15 +28,25 @@ const Navbar: FC = () => {
       </Button>
       <div className='flex items-center gap-2'>
         <button
-          className='text-slate-300 transition-colors hover:text-slate-50'
-          //TODO notifications
+          className='relative text-slate-300 transition-colors hover:text-slate-50'
+          aria-label='Notifications'
+          // TODO: notifications
         >
-          {/*//TODO notification counter */}
+          <div
+            className={cn(
+              'absolute top-0 right-0 hidden h-2 w-2 items-center justify-center rounded-full bg-sky-400 text-xs text-white',
+              {
+                // TODO: enable if user has unread notifications
+                flex: false,
+              }
+            )}
+          />
           <BellIcon className='h-5' />
         </button>
         <button
           className='text-slate-300 transition-colors hover:text-slate-50'
           onClick={openSettings}
+          aria-label='Settings'
         >
           <Cog8ToothIcon className='h-5' />
         </button>
