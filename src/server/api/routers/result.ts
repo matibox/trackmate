a7ed00bfa5f6353ac1516cc8b9ad86e16e43984d
Matibox <mateusz.hladky@gmail.com>
2023-03-01 21:56:08 +0100

    Add new result notification server side implementation

--- src/server/api/routers/result.ts
+++ src/server/api/routers/result.ts
@@ -16,15 +16,29 @@ export const resultRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const { eventId, ...fields } = input;
-      return await ctx.prisma.result.create({
-        data: {
-          ...fields,
-          event: { connect: { id: eventId } },
-          team: hasRole(ctx.session, 'manager')
-            ? { connect: { managerId: ctx.session.user.id } }
-            : { connect: { id: ctx.session.user.teamId ?? undefined } },
-          author: { connect: { id: ctx.session.user.id } },
-        },
+      return await ctx.prisma.$transaction(async tx => {
+        const result = await tx.result.create({
+          data: {
+            ...fields,
+            event: { connect: { id: eventId } },
+            team: hasRole(ctx.session, 'manager')
+              ? { connect: { managerId: ctx.session.user.id } }
+              : { connect: { id: ctx.session.user.teamId ?? undefined } },
+            author: { connect: { id: ctx.session.user.id } },
+          },
+          include: { team: { select: { managerId: true } } },
+        });
+        return await tx.newResultNotification.create({
+          data: {
+            message: 'New result has been posted',
+            receiver: {
+              connect: { id: result.team.managerId },
+            },
+            result: {
+              connect: { id: result.id },
+            },
+          },
+        });
       });
     }),
   getResultPage: multiRoleProcedure(['manager', 'socialMedia'])
