cb4e804d2a31045b03103afa01591dbe6d08137c
Matibox <mateusz.hladky@gmail.com>
2023-03-31 19:18:56 +0200

    Map through setups

--- src/pages/setups.tsx
+++ src/pages/setups.tsx
@@ -6,9 +6,10 @@ import Loading from '@ui/Loading';
 import Tile from '@ui/Tile';
 import { type NextPage } from 'next';
 import { NextSeo } from 'next-seo';
+import { type FC } from 'react';
 import { useError } from '../hooks/useError';
 import { usePostSetupStore } from '../store/usePostSetupStore';
-import { api } from '../utils/api';
+import { api, type RouterOutputs } from '../utils/api';
 
 const YourSetups: NextPage = () => {
   const { Error, setError } = useError();
@@ -17,6 +18,8 @@ const YourSetups: NextPage = () => {
   });
 
   const { open } = usePostSetupStore();
+
+  // TODO: setup sorting/filtering
 
   return (
     <>
@@ -32,13 +35,13 @@ const YourSetups: NextPage = () => {
           Your Setups
         </h1>
         <div className='flex flex-col gap-4 md:flex-row'>
-          <Tile className='md:basis-1/4 lg:basis-1/5'>
+          <Tile className='md:basis-1/3 lg:basis-1/4 xl:basis-1/5'>
             <div className='flex w-full flex-col gap-4'>
               <label className='flex w-full items-center gap-2'>
                 <MagnifyingGlassIcon className='h-5' />
                 <Input
                   className='h-7'
-                  placeholder='Search setup'
+                  placeholder='Name, car, track or author'
                   error={undefined}
                 />
               </label>
@@ -46,7 +49,7 @@ const YourSetups: NextPage = () => {
                 intent='primary'
                 size='small'
                 gap='small'
-                className='ml-auto self-end'
+                className='mt-auto self-end'
                 onClick={open}
               >
                 <span>New setup</span>
@@ -54,11 +57,25 @@ const YourSetups: NextPage = () => {
               </Button>
             </div>
           </Tile>
-          <Tile className='flex-1'>no</Tile>
+          <Tile className='flex-1'>
+            {setups?.map(setup => (
+              <Setup key={setup.id} setup={setup} />
+            ))}
+            {setups?.length === 0 && !isLoading && (
+              <span className='text-slate-300'>There are no setups</span>
+            )}
+          </Tile>
         </div>
       </main>
     </>
   );
+};
+
+const Setup: FC<{ setup: RouterOutputs['setup']['getAll'][number] }> = ({
+  setup,
+}) => {
+  // TODO: setup component
+  return <div>{setup.name}</div>;
 };
 
 export default YourSetups;
--- src/server/api/routers/setup.ts
+++ src/server/api/routers/setup.ts
@@ -39,6 +39,7 @@ export const setupRouter = createTRPCRouter({
           },
         ],
       },
+      orderBy: { updatedAt: 'desc' },
     });
   }),
 });
