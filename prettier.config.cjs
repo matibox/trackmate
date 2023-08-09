419698fe7a96f6c1e86b5369f95a54eb27910b8f
Matibox <mateusz.hladky@gmail.com>
2023-08-09 14:40:45 +0200

    Reset

--- prettier.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  singleQuote: true,
-  jsxSingleQuote: true,
-  arrowParens: "avoid",
-};
