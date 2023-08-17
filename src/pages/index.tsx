316558c6e9c2f7f59fab355a09016dea2ef33e0c
Matibox <mateusz.hladky@gmail.com>
2023-08-18 00:11:06 +0200

    Remove preview

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
 import { type NextPage } from 'next';
-import { Button } from '~/components/ui/Button';
 
 const Home: NextPage = () => {
-  return (
-    <div className='absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 gap-4'>
-      <Button>Primary</Button>
-      <Button intent='secondary'>Secondary</Button>
-      <Button intent='positive'>Positive</Button>
-      <Button intent='destructive'>Destructive</Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
