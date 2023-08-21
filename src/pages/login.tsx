a60d16923f3b334ef1ed8cd70cd162cd702f9cc2
Matibox <mateusz.hladky@gmail.com>
2023-08-21 23:12:25 +0200

    Add login page, downgrade lucide-react to working version

---
+++ src/pages/login.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from 'next';
+import { X } from 'lucide-react';
+
+const Login: NextPage = () => {
+  return (
+    <div className='relative flex h-screen flex-col'>
+      <header>
+        <X />
+      </header>
+    </div>
+  );
+};
+
+export default Login;
