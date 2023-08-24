9c44b61b9efb6c3855623986e21f4b4de633b992
Matibox <mateusz.hladky@gmail.com>
2023-08-24 02:05:20 +0200

    Add show password toggling

--- src/core/welcome/components/StepThree.tsx
+++ src/core/welcome/components/StepThree.tsx
@@ -12,14 +12,15 @@ import { Button } from '~/components/ui/Button';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useWelcomeForm } from '../store/formStore';
 import WelcomeLayout from './Layout';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/Tabs';
+import { useState } from 'react';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
 
 const createTeamSchema = z.object({
   teamName: z.string({ required_error: 'Team name is required.' }),
   abbreviation: z
-    .string()
+    .string({ required_error: 'Abbreviation is required.' })
     .length(3, 'Abbreviation needs to be exactly 3 characters long.'),
   password: z
     .string()
@@ -27,6 +28,8 @@ const createTeamSchema = z.object({
 });
 
 export default function StepOne() {
+  const [showPassword, setShowPassword] = useState(false);
+
   const createTeamForm = useForm<z.infer<typeof createTeamSchema>>({
     resolver: zodResolver(createTeamSchema),
   });
@@ -40,7 +43,7 @@ export default function StepOne() {
       title='Welcome to TrackMate'
       description='Fill in your data to continue with the app. We use your name only for displaying purposes.'
     >
-      <Tabs defaultValue='create' className='w-[384px]'>
+      <Tabs defaultValue='create' className='w-full max-w-sm'>
         <TabsList className='mb-7 grid w-full grid-cols-2 md:mb-9 xl:mb-16'>
           <TabsTrigger value='create'>Create team</TabsTrigger>
           <TabsTrigger value='join'>Join existing team</TabsTrigger>
@@ -82,11 +85,25 @@ export default function StepOne() {
                 control={createTeamForm.control}
                 name='password'
                 render={({ field }) => (
-                  <FormItem>
+                  <FormItem className='relative'>
                     <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input type='password' {...field} />
-                    </FormControl>
+                    <div className='relative'>
+                      <FormControl>
+                        <Input
+                          type={showPassword ? 'text' : 'password'}
+                          {...field}
+                        />
+                      </FormControl>
+                      <Button
+                        variant='ghost'
+                        size='icon'
+                        type='button'
+                        className='absolute right-0 top-1/2 -translate-y-1/2 rounded-l-none border-b border-r border-t border-slate-800 bg-slate-950'
+                        onClick={() => setShowPassword(prev => !prev)}
+                      >
+                        {showPassword ? <EyeIcon /> : <EyeOffIcon />}
+                      </Button>
+                    </div>
                     <FormDescription>
                       Everyone who knows this password will be able to join the
                       team.
@@ -98,6 +115,9 @@ export default function StepOne() {
               <Button type='submit'>Create team</Button>
             </form>
           </Form>
+        </TabsContent>
+        <TabsContent value='join'>
+          <div className='text-center'>Coming soon&trade;</div>
         </TabsContent>
       </Tabs>
     </WelcomeLayout>
