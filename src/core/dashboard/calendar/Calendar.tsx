e8d8ed39b1a664c3720fd98057f231faaa741790
Matibox <mateusz.hladky@gmail.com>
2023-10-14 19:12:03 +0200

    Add displaying of calendar days

--- src/core/dashboard/calendar/Calendar.tsx
+++ src/core/dashboard/calendar/Calendar.tsx
@@ -1,7 +1,11 @@
+import dayjs from 'dayjs';
 import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
 import { Button } from '~/components/ui/Button';
+import { generateDayGrid } from '~/lib/dates';
 
 export default function Calendar() {
+  const dayGrid = generateDayGrid(10 - 1, 2023);
+
   return (
     <section className='w-full max-w-lg rounded-md bg-slate-900 ring-1 ring-slate-800'>
       <header className='outline-b-1 flex w-full items-center justify-between border-b border-slate-800 px-4 py-2'>
@@ -13,7 +17,33 @@ export default function Calendar() {
           <ChevronRightIcon className='h-5 w-5' />
         </Button>
       </header>
-      <div className='px-4 py-2'>days</div>
+      <div className='flex w-full flex-col gap-3 px-4 py-2'>
+        <div className='flex gap-3'>
+          {dayGrid[0]?.map((day, i) => (
+            <span
+              key={i}
+              className='flex h-full w-full items-center justify-center text-sm font-medium text-slate-300'
+            >
+              {dayjs(day).format('dd')}
+            </span>
+          ))}
+        </div>
+        {dayGrid.map((row, i) => (
+          <div key={i} className='flex h-[37px] gap-3'>
+            {row.map((day, i) => (
+              <Day key={i} day={day} />
+            ))}
+          </div>
+        ))}
+      </div>
     </section>
+  );
+}
+
+function Day({ day }: { day: dayjs.Dayjs }) {
+  return (
+    <div className='bg-text-sm flex h-full w-full items-center justify-center rounded-md'>
+      {day.date()}
+    </div>
   );
 }
