41c938074dd343b3a27a99dbdb84552b3508976a
Matibox <mateusz.hladky@gmail.com>
2023-10-14 19:34:57 +0200

    Add calendar store, add month switch

---
+++ src/core/dashboard/calendar/store.ts
@@ -0,0 +1,21 @@
+import dayjs, { type Dayjs } from 'dayjs';
+import { subscribeWithSelector } from 'zustand/middleware';
+import { create } from 'zustand';
+
+type CalendarStore = {
+  currentDay: Dayjs;
+  getCurrentMonthIndex: () => number;
+  nextMonth: () => void;
+  prevMonth: () => void;
+};
+
+export const useCalendar = create<CalendarStore>()(
+  subscribeWithSelector((set, get) => ({
+    currentDay: dayjs(),
+    getCurrentMonthIndex: () => get().currentDay.month(),
+    nextMonth: () =>
+      set(state => ({ currentDay: state.currentDay.add(1, 'month') })),
+    prevMonth: () =>
+      set(state => ({ currentDay: state.currentDay.subtract(1, 'month') })),
+  }))
+);
