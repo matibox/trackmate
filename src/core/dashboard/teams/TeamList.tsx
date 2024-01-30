fb80cb7244c0fde6536b73173c01d05b8e37431a
Matibox <mateusz.hladky@gmail.com>
2024-01-30 15:33:52 +0100

    add loading skeleton

--- src/core/dashboard/teams/TeamList.tsx
+++ src/core/dashboard/teams/TeamList.tsx
@@ -5,6 +5,7 @@ import Team from './Team';
 import { Button } from '~/components/ui/Button';
 import { useNewTeam } from './new-team/store/newTeamStore';
 import { PlusIcon } from 'lucide-react';
+import { Skeleton } from '~/components/ui/Skeleton';
 
 export default function TeamList() {
   const { data: teams, status, error } = api.team.list.useQuery();
@@ -22,7 +23,30 @@ export default function TeamList() {
   }, [error?.message, status, toast]);
 
   if (status === 'loading') {
-    return <section className='flex w-full flex-col'>TODO: skeleton</section>;
+    return (
+      <section className='grid grid-cols-1 gap-4 md:grid-cols-[repeat(auto-fill,_417px)]'>
+        {Array(2)
+          .fill(null)
+          .map((_, i) => (
+            <div
+              key={i}
+              className='flex w-full items-center gap-4 rounded-md p-4 ring-1 ring-slate-800'
+            >
+              <Skeleton className='h-10 w-10 rounded-full' />
+              <div className='flex flex-col gap-2'>
+                <Skeleton className='h-3 w-40' />
+                <div className='flex gap-2'>
+                  {Array(2)
+                    .fill(null)
+                    .map((_, i) => (
+                      <Skeleton key={i} className='h-5 w-10' />
+                    ))}
+                </div>
+              </div>
+            </div>
+          ))}
+      </section>
+    );
   }
 
   if (status === 'error') {
