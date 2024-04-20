64087ad62c141a3a85b3adadde50d3083033848c
Matibox <mateusz.hladky@gmail.com>
2024-04-20 15:47:40 +0200

    Remove ring from Team component

--- src/core/dashboard/teams/Team.tsx
+++ src/core/dashboard/teams/Team.tsx
@@ -23,7 +23,7 @@ export default function Team({ team }: { team: Team }) {
   }, [team.members, team.rosters]);
 
   return (
-    <div className='flex w-full items-center gap-4 rounded-md bg-slate-900 p-4 ring-1 ring-slate-800'>
+    <div className='flex w-full items-center gap-4 rounded-md bg-slate-900 p-4'>
       <Avatar>
         <AvatarImage
           src={team.profilePicture ?? ''}
