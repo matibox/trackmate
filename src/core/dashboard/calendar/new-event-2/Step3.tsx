fa1e2270d58877483179a5eddd91b2d2c7824a0c
Matibox <mateusz.hladky@gmail.com>
2024-06-12 18:19:59 +0200

    Add new session dropdown menu

--- src/core/dashboard/calendar/new-event-2/Step3.tsx
+++ src/core/dashboard/calendar/new-event-2/Step3.tsx
@@ -1,10 +1,29 @@
+import { PlusIcon } from 'lucide-react';
+import { ReactNode } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { z } from 'zod';
+import { Button } from '~/components/ui/Button';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/Dialog';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '~/components/ui/DropdownMenu';
 import {
   SheetDescription,
   SheetHeader,
   SheetTitle,
 } from '~/components/ui/Sheet';
+import { sessionTypes } from '~/lib/constants';
+import { capitalize } from '~/lib/utils';
 
 export const stepThreeSchema = z.object({
   // schema
@@ -22,7 +41,38 @@ export default function StepThree() {
         </SheetDescription>
       </SheetHeader>
       <div className='mx-auto flex w-4/5 flex-col gap-4 py-8 text-slate-50'>
-        {/* INSERT CODE HERE */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant='secondary'
+              className='justify-between'
+              // disabled={loading}
+            >
+              New session
+              <PlusIcon className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className='w-[268px]'>
+            {sessionTypes.map(sessionType => (
+              <Dialog key={sessionType}>
+                <DialogTrigger asChild>
+                  <DropdownMenuItem onSelect={e => e.preventDefault()}>
+                    {capitalize(sessionType)}
+                  </DropdownMenuItem>
+                </DialogTrigger>
+                <DialogContent className='text-slate-50'>
+                  <DialogHeader>
+                    <DialogTitle>Create {sessionType} session</DialogTitle>
+                    <div className='flex w-full flex-col gap-4 text-slate-50'></div>
+                  </DialogHeader>
+                  <DialogFooter>
+                    <Button>Submit</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </>
   );
